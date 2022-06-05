7ee70733fc12222add9dc3af4801b4ab3b4e5ebe
Chasti <chastytim@mail.ru>
2022-06-06 00:21:42 +0300

    end with login codes

--- modules/views.py
+++ modules/views.py
@@ -12,9 +12,7 @@ import base64
 
 
 bp = Blueprint('views', __name__)
-
-
-keyPair = RSA.generate(1024)
+key_list = []
 
 
 @bp.route("/test")
@@ -47,47 +45,47 @@ def registration():
 
 @bp.route("/login", methods=['GET', 'POST'])
 def login():
+    global key_list
     form = forms.LoginForm()
     ermessage = ''
-    global keyPair
+    keyPair = RSA.generate(1024)
     public_key = keyPair.publickey()
     pubKey = public_key.exportKey().decode('ascii')
-    privKey = keyPair.exportKey().decode('ascii')
-
-    # print(pubKey, privKey)
-
+    key_list.append(
+        {
+            "public": pubKey,
+            "key_pair": keyPair
+        }
+    )
     if form.validate_on_submit():
-        # session['name'] = form.name.data
-        # session['password'] = form.password.data
-        # try:
-        # sleep(1)
-        # print(form.password.data)
-        """
-        ciphertext = base64.b64decode(form.password.data)
-        cipher = PKCS1_OAEP.new(keyPair)
-        print(ciphertext)
-        # data_bytes = base64.b64decode(str(ciphertext).encode('utf-8'))
-        # print(form.password.data)
-        plain_text = cipher.decrypt(ciphertext)
-        print(base64.b64encode(plain_text))
-            # return redirect('/')
-        # except Exception as e:
-        #    ermessage = 'Ошибка - попытайтесь снова'
-        """
+        return render_template('login.html', form=form, error=ermessage, key=pubKey)
     return render_template('login.html', form=form, error=ermessage, key=pubKey)
 
 
 @bp.route("/jscipher", methods=['GET', 'POST'])
 def jscipher():
     data = request.get_json(force=True)
-    print(data)
-    global keyPair
-    ciphertext = base64.b64decode(data)
-    cipher = PKCS1_v1_5.new(keyPair)
-    plain_text = cipher.decrypt(ciphertext, 'bollox')
-    print(plain_text, 'text')
-    return (
-        json.dumps({'info': 'ok'}),
-        200,
-        {'ContentType': 'application/json'}
-    )
+    global key_list
+    key_Pair = None
+    print(key_list)
+    for i in key_list:
+        if data["public_key"][27:-25].replace("\n", "") == i["public"][27:-25].replace("\n", ""):
+            key_Pair = i['key_pair']
+            break
+    if key_Pair:
+        ciphertext = base64.b64decode(data["Ciphertext"])
+        cipher = PKCS1_v1_5.new(key_Pair)
+        plain_text = cipher.decrypt(ciphertext, 'bollox')
+        print(plain_text, 'text')
+        return (
+            json.dumps({'info': 'ok'}),
+            200,
+            {'ContentType': 'application/json'}
+        )
+    else:
+        print('not found', data["public_key"][27:-25])
+        return (
+            json.dumps({'info': 'keypain not found'}),
+            200,
+            {'ContentType': 'application/json'}
+        )
