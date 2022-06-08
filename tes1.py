1c1998ece2bcbc8b8fd8f2f3cf007585cc1ae512
Chasti <chastytim@mail.ru>
2022-06-09 01:37:33 +0300

    end tests

--- tes1.py
+++ tes1.py
@@ -1,3 +1,5 @@
+from lib2to3.pgen2 import token
+from logging import exception
 from urllib import response
 from Cryptodome.PublicKey import RSA
 from Cryptodome.Cipher import PKCS1_v1_5
@@ -6,17 +8,13 @@ import base64
 import base64
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding, utils
-from cryptography.hazmat.primitives.serialization import load_pem_public_key, load_pem_private_key
+from cryptography.hazmat.primitives.serialization import load_pem_public_key
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
 from cryptography.hazmat.backends import default_backend
 import hashlib
 import uuid
 
 import requests
-import json
-
-"""
------BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCmzbPdc0zZclXXdHTBCde4CFfD\n3M6ejqzNUnCfa46lEKO8+41IQJuWBVCXebjpkcBZydKqlcvSfkK1p9gnCzx2lCjs\n6gC9sIPt1vuLBd8MxlRlcrPzku+3TU6xkdIb6AZCe9W6c6hW+9H+uOi5qdKZtggq\nChfKhMkGkO9790XeYQIDAQAB\n-----END PUBLIC KEY-----"
-"""
 
 
 def ecryptor(text, key):
@@ -92,13 +90,14 @@ def send_message(username, recipient, text, token, keypair, server_key):
     )
     recipient_url = f'{url}getuserkeypem?recipient={recipient}'
     recipient_key = requests.get(url=recipient_url).json()['openkey']  # x
-    print(recipient_key)
     recipient_key = load_pem_public_key(
         str.encode(recipient_key),
         backend=default_backend()
     )
     en_message = ecryptor(f'{username}|{text}', recipient_key)
-    hash = hashlib.md5(str.encode(f'{token}{en_message}{server_token}')).hexdigest().upper()
+    hash = hashlib.md5(
+        str.encode(f'{token}{en_message}{server_token}')
+    ).hexdigest().upper()
     data = {
         'sender': hashlib.md5(str.encode(username)).hexdigest().upper(),
         'recipient': hashlib.md5(str.encode(recipient)).hexdigest().upper(),
@@ -109,12 +108,94 @@ def send_message(username, recipient, text, token, keypair, server_key):
     print(response.json())
 
 
+def authorization(username, password, keypair, server_key):
+    url = 'http://178.21.10.180/authorization'
+    random_str = str(uuid.uuid4())
+    server_token = get_token(
+        operation_id=1,
+        username=username,
+        keyPair=keypair,
+        random_str=random_str,
+        server_key=server_key
+    )
+    user_name = ecryptor(f'{server_token}|{username}|{random_str}', server_key)
+    user_pass = ecryptor(f'{random_str}|{password}|{server_token}', server_key)
+    data = {
+        "name": user_name,
+        "openkey": str(keypair.publickey().exportKey().decode('ascii')),
+        "password": user_pass
+    }
+    response = requests.post(url=url, json=data)
+    token = response.json()['token']
+    cipher = PKCS1_v1_5.new(keyPair)
+    sentinel = get_random_bytes(20)
+    plain_text = cipher.decrypt(
+        base64.b64decode(str(token).encode('utf-8')),
+        sentinel
+    )
+    return plain_text.decode("utf-8").split('|')[1]
+
+
+def get_messages(username, token, keypair, server_key):
+    url = 'http://178.21.10.180/getmessages'
+    random_str = str(uuid.uuid4())
+    server_token = get_token(
+        operation_id=3,
+        username=username,
+        keyPair=keypair,
+        random_str=random_str,
+        server_key=server_key
+    )
+    hash_name = hashlib.md5(str.encode(username)).hexdigest().upper()
+    hash = hashlib.md5(
+        str.encode(f'{token}{hash_name}{server_token}')
+    ).hexdigest().upper()
+    data = {
+        'name': hashlib.md5(str.encode(username)).hexdigest().upper(),
+        'openkey': str(keypair.publickey().exportKey().decode('ascii')),
+        'hashkey': hash,
+    }
+    response = requests.post(url=url, json=data)
+    try:
+        messages = response.json()['gettedmessages']
+    except exception as e:
+        print(e)
+    if messages:
+        print(messages)
+        cipher = PKCS1_v1_5.new(keyPair)
+        sentinel = get_random_bytes(20)
+        for i in messages.split('#'):
+            print(str(i.split('|')[-1]))
+            plain_text = cipher.decrypt(
+                base64.b64decode(str(i.split('|')[-1]).encode('utf-8')),
+                sentinel
+            )
+            print(plain_text.decode("utf-8"))
+    """
+    token = response.json()['token']
+    cipher = PKCS1_v1_5.new(keyPair)
+    sentinel = get_random_bytes(20)
+    plain_text = cipher.decrypt(
+        base64.b64decode(str(token).encode('utf-8')),
+        sentinel
+    )
+    return plain_text.decode("utf-8").split('|')[1]
+    """
+
+
 keyPair = RSA.generate(1024)
+keyPair1 = RSA.generate(1024)
 server_key = load_pem_public_key(
         open('server_public.pem').read().encode(),
         backend=default_backend()
 )
-# token = registration('test', 'qwerty', keyPair, server_key)
+
+token1 = authorization('test', 'qwerty', keyPair, server_key)
+
+token = 'c244d66c-1d66-4060-90be-7e49ee9d6e67'
+send_message('test1', 'test', 'test message 2', token, keyPair, server_key)
+
+get_messages('test', token1, keyPair, server_key)
+
+# token = registration('test1', 'qwerty', keyPair, server_key)
 # print(token)
-token = 'b0e083ad-d5bc-4444-a07b-c6a90e6db5f0'
-send_message('test', 'gfd', '121231sdfgss', token, keyPair, server_key)
